a814b0d72dee4b6f729212bd5f02fd3fb499d461
alexandermarcell <alexandermarcell36@gmail.com>
2022-02-27 19:33:05 -0500

    testing

--- src/components/CurrentVideo/CurrentVideo.jsx
+++ src/components/CurrentVideo/CurrentVideo.jsx
@@ -1,6 +1,8 @@
 import './CurrentVideo.scss';
 import Likes from '../../Assets /Icons/likes.svg';
 import Views from '../../Assets /Icons/views.svg';
+import Button from '../Button/Button';
+import Avatar from '../Avatar/Avatar';
 
 function CurrentVideo({currentVideo}){
     return (
@@ -45,6 +47,20 @@ function CurrentVideo({currentVideo}){
                     <p className='current-section__wrap__description__text'>
                         {currentVideo.description}
                     </p>
+                </div>
+                <div className="current-section__wrap__comments">
+                    <Avatar/>
+                    <div className="current-section__wrap__comments-container">
+                        <form
+                        className='current-section__wrap__comments-form'
+                        action="Get"
+                        >
+                            <label className='current-section__wrap__comment-form__label' htmlFor="/">Join The Conversation</label>
+                            <textarea name="comment" id="form-textarea" placeholder='Add a new comment' cols="30" rows="10"></textarea>
+                        </form>
+                        <Button/>
+                    </div>
+                    
                 </div>
             </div>
         </section>
